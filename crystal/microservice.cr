1de96ba5a09de8ac5fc064774f205f821edb1680
Stephen Solka <stephen0q@gmail.com>
2018-08-22 17:00:39 -0400

    clean up

--- crystal/microservice.cr
+++ crystal/microservice.cr
@@ -4,12 +4,14 @@ require "kemal"
 
 get "/" do
   response = HTTP::Client.get "https://www.reddit.com/r/politics/hot.json"
-  reddit = JSON.parse(response.body)
+  reddit = JSON.parse response.body
   children = reddit["data"]["children"].as_a.map { |l| l["data"] }
 
   JSON.build do |json|
     json.array do
-      children.map { |c| json.field "title", c["title"] }
+      children.map do |c|
+        json.field "title", c["title"]
+      end
     end
   end
 end
